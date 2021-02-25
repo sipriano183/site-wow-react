8dfaf1be7ecee494f98dfb4e6c6e32a1f495edc2
sipriano183 <77734544+sipriano183@users.noreply.github.com>
2021-02-25 16:25:21 -0300

    ..

--- src/data/professions.js
+++ src/data/professions.js
@@ -2,62 +2,62 @@ const professions = [
     {
         id:0,
         name:"Alchemy",
-        image: require("../assets/images/trade/Trade_Alchemy.png")
+        image: require("../assets/images/Trade/alchemy.png")
     },
     {
         id:1,
         name:"Blacksmithing",
-        image: require("../assets/images/trade/Trade_BlackSmithing.png")
+        image: require("../assets/images/Trade/blacksmithing.png")
     },
     {
         id:2,
         name:"Cooking",
-        image: require("../assets/images/trade/INV_Misc_Food_15.png")
+        image: require("../assets/images/Miscellaneous/Food_15.png")
     },
     {
         id:3,
         name:"Enchanting",
-        image: require("../assets/images/trade/Trade_Engraving.png")
+        image: require("../assets/images/Trade/engraving.png")
     },
     {
         id:4,
         name:"Engineering",
-        image: require("../assets/images/trade/Trade_Engineering.png")
+        image: require("../assets/images/Trade/engineering.png")
     },
     {
         id:5,
         name:"Fishing",
-        image: require("../assets/images/trade/Trade_Fishing.png")
+        image: require("../assets/images/Miscellaneous/Net_01.png")
     },
     {
         id:6,
         name:"Leatherworking",
-        image: require("../assets/images/trade/INV_Misc_LeatherScrap_03.png")
+        image: require("../assets/images/Trade/leatherworking.png")
     },
     {
         id:7,
         name:"First Aid",
-        image: require("../assets/images/spells/Spell_Holy_SealOfSacrifice.png")
+        image: require("../assets/images/Spells/SealOfSacrifice.png")
     },
     {
         id:8,
         name:"Mining",
-        image: require("../assets/images/trade/Trade_Mining.png")
+        image: require("../assets/images/Trade/mining.png")
     },
     {
         id:9,
         name:"Herbalism",
-        image: require("../assets/images/trade/Trade_Herbalism.png")
+        image: require("../assets/images/Trade/herbalism.png")
     },
     {
         id: 10,
         name:"Skinning",
-        image: require("../assets/images/trade/INV_Misc_Pelt_Wolf_01.png")
+        image: require("../assets/images/Miscellaneous/Pelt_Wolf_01.png")
     },
     {
         id:11,
         name:"Tailoring",
-        image: require("../assets/images/trade/Trade_Tailoring.png")
+        image: require("../assets/images/Trade/tailoring.png")
     }
 
 ] 
